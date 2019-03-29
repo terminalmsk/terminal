1e22a207c4a55dfe10150a465e7d5349cade9419
pvv <pvv@equifax.ru>
2019-03-29 16:13:24 +0300

    ajax

---
+++ DISHAMOUNTINT.php
@@ -0,0 +1,59 @@
+<?php
+echo '{"number":"---","msg":"Client error response\n[status code] 403\n[reason phrase] Forbidden\n[url] http:\/\/sr1.resto-service.ru:18001\/resto\/j_spring_security_check"}';
+die();
+error_reporting(E_ERROR);
+ini_set('display_errors', 1);
+
+use Guzzle\Plugin\Cookie\CookieJar\FileCookieJar;
+use Guzzle\Plugin\Cookie\CookiePlugin;
+
+try {
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $cookie_file_name = __DIR__ . '/test.cookie';
+    $cookiePlugin = new CookiePlugin(new FileCookieJar($cookie_file_name));
+
+
+    $client = new \Guzzle\Http\Client([
+        'headers' => [
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
+        ],
+    ]);
+    $client->addSubscriber($cookiePlugin);
+
+    $request = $client->createRequest(
+        'POST',
+        'http://sr1.resto-service.ru:18001/resto/j_spring_security_check',
+        [], [
+        'j_username' => 'web',
+        'j_password' => '45674567',
+    ],
+        []);
+
+    $response = $request->send();
+    $now = new DateTime();
+
+    $dateFormat = 'd.m.Y';
+    $urlParams = [
+        'dateFrom' => $now->format($dateFormat),
+        'dateTo' => $now->add(DateInterval::createFromDateString('day'))->format($dateFormat),
+        'presetId' => '8b64c3a3-391c-4dc6-abb7-5bb85627198a',
+    ];
+
+    $request = $client->createRequest('GET', 'http://sr1.resto-service.ru:18001/resto/service/reports/report.jspx?' . http_build_query($urlParams));
+
+    $response = $request->send();
+
+    $xmlRaw = $response->getBody(1);
+
+    $p = xml_parser_create();
+    xml_parse_into_struct($p, $xmlRaw, $vals, $index);
+    xml_parser_free($p);
+
+    $value = ceil($vals[$index['DISHAMOUNTINT'][0]]['value'] ?: false);
+
+} catch (Exception $e) {
+    $value = "---";
+    $msg = $e->getMessage();
+}
+echo json_encode(["number" => $value, 'msg' => $msg]);
--- index.php
+++ index.php
@@ -1,58 +1,34 @@
-<?php
+<html>
+<head>
 
-use Guzzle\Plugin\Cookie\CookieJar\FileCookieJar;
-use Guzzle\Plugin\Cookie\CookiePlugin;
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script>
+        function LoopData() {
+            setTimeout(function () {
+                renewData();
+                LoopData();
+            }, 1000)
+        }
 
-try {
-    require_once __DIR__ . '/vendor/autoload.php';
+        function renewData() {
+            $.ajax({
+                url: "DISHAMOUNTINT.php",
+                context: document.body
+            }).done(function (data) {
+                var obj = jQuery.parseJSON(data);
+                $("#data-field-msg").text(obj.msg);
+                $("#data-field-number").text(obj.number);
+            });
+        }
 
-    $cookie_file_name = __DIR__ . '/test.cookie';
-    $cookiePlugin = new CookiePlugin(new FileCookieJar($cookie_file_name));
+        $(document).ready(
+            LoopData()
+        );
+    </script>
+</head>
+<body>
+<h1>За сегодня мы приготовили <span id="data-field-number"></span></h1>
 
-
-    $client = new \Guzzle\Http\Client([
-        'headers' => [
-            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0'
-        ],
-    ]);
-    $client->addSubscriber($cookiePlugin);
-
-    $request = $client->createRequest(
-        'POST',
-        'http://sr1.resto-service.ru:18001/resto/j_spring_security_check',
-        [], [
-        'j_username' => 'web',
-        'j_password' => '999999',
-    ],
-        []);
-
-    $response = $request->send();
-
-
-    $now = new DateTime();
-
-    $dateFormat = 'd.m.Y';
-
-    $urlParams = [
-        'dateFrom' => $now->format($dateFormat),
-        'dateTo' => $now->add(DateInterval::createFromDateString('day'))->format($dateFormat),
-        'presetId' => '8b64c3a3-391c-4dc6-abb7-5bb85627198a',
-    ];
-
-    $request = $client->createRequest('GET', 'http://sr1.resto-service.ru:18001/resto/service/reports/report.jspx?' . http_build_query($urlParams));
-
-    $response = $request->send();
-
-    $xmlRaw = $response->getBody(1);
-
-    $p = xml_parser_create();
-    xml_parse_into_struct($p, $xmlRaw, $vals, $index);
-    xml_parser_free($p);
-
-    $value = ceil($vals[$index['DISHAMOUNTINT'][0]]['value'] ?: false);
-    echo '<h1>ЗА сегодня было приготовлено вот столько '. ($value).'</h1>>';
-
-} catch (Exception $e) {
-var_dump($e);
-
-}
+<span style="font-size: smaller;color:red;" id="data-field-msg"></span>
+</body>
+</html>
